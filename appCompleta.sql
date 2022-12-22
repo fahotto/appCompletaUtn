59a5edff05ef77f14e5254518db8d881bd4d8094
Dante <dfahotto@gmail.com>
2022-12-22 16:06:03 -0300

    appcompleta

--- appCompleta.sql
+++ appCompleta.sql
@@ -1,15 +1,32 @@
+-- Database para la App Completa --
+create database empresautn;
+
+-- Selecciono la Database -- 
 use empresautn;
 
+-- drop table empleados; --
+-- drop table contactos; --
+
+
+-- Creamos la tabla Productos para la App Completa --
 create table productos(
 idProducto int unsigned not null auto_increment,
 nombre varchar(150) not null,
 precio int not null,
-descripcion varchar (200) not null,
-primary key (idProducto));
--- creamos la tabala contacto para la app completa
+descripcion varchar(200) not null,
+primary key (idProducto)
+);
 
-create table contacto(
+-- Creamos la tabla Contacto para la App Completa --
+create table contactos(
 idContacto int unsigned not null auto_increment,
 nombre varchar(150) not null,
-email varchar (100) not null,
-primary key (idContacto));
+email varchar(150) not null,
+primary key (idContacto)
+);
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 const express = require('express');
-
 const app = express();
 const mysql = require('mysql2');
-
-//motor de plantilla
+//Motor de plantilla
 const hbs = require('hbs');
-//encontrar archivos
+//Encontrar archivos
 const path = require('path');
-//enviar mails
+//Para enviar mails
 const nodemailer = require('nodemailer');
-const { appendFile, read } = require('fs');
-
-//variables de entorno
+//Variables de entorno
 require('dotenv').config();
 
-//configuramos el puerto
-const APP_PORT = process.env.APP_PORT 
+//Configuramos el Puerto
+const PORT = process.env.PORT || 9000; 
 
-//middelware funciones que dan info a json
-
+//Middelware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
 
-
-//configuramos el motor de plantillas de HBS
+//Configuramos el motor de plantillas de HBS
 app.set('view engine', 'hbs');
-
-//configuramos la ubicacion de las plantillas
+//Configuramos la ubicación de las plantillas
 app.set('views', path.join(__dirname, 'views'));
-
-//configuramos los parciales de los motores de plantillas
+//Configuramos los parciales de los motores de plantillas
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 //Conexión a la Base de Datos
@@ -39,83 +31,90 @@ const conexion = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_DBPORT
 })
 
 conexion.connect((err) =>{
     if(err) throw err;
     console.log(`Conectado a la Database ${process.env.MYSQL_DATABASE}`);
-})
+}) 
 
-//rutas de la aplicacion
-
-
-
+//Rutas de la Aplicación
 app.get('/', (req, res) => {
     res.render('index', {
-        titulo: 'main'
+        titulo: 'Home'
     })
-});
-
+})
+ 
 app.get('/formulario', (req, res) => {
     res.render('formulario')
-});
+})
 
 app.get('/productos', (req, res) => {
     
-    let SQL = "SELECT * from productos";
-        conexion.query(SQL, function(err, result){
-            if (err) throw err;
-                console.log(result);
-                res.render('productos', {
-                titulo: 'Productos',
-                datos: result
-                })
-        })
-        
-    });
+    let sql = "SELECT * FROM productos";
 
-app.get('/contacto', (req, res) => {
+    conexion.query(sql, function(err, result){
+        if (err) throw err;
+        console.log(result);
+        res.render('productos', {
+            titulo: 'Productos',
+            datos: result
+        })
+    })
+})
+
+app.get('/contacto', (req, res) =>{
     res.render('contacto', {
         titulo: 'Contacto'
     })
-});
+})
 
-app.get('/datepicker', (req, res) => {
-    res.render('./datepicker', {
-        titulo: 'datepicker'
+
+/* TERMINAR DE HACER EL LOGIN
+app.get('/login', (req, res) =>{
+    const user = req.body.user;
+    const password = req.body.password;
+
+    console.log(`${user}` - `${password}`)
+
+    if (user === 'admin' & password === 'dante') {
+        res.render('admin')
+    } else {
+        res.render('error')
+    }
     })
-});
- 
-app.post('/formulario', (req, res) => {
-    console.log(req);
+*/
+
+
+app.post('/formulario', (req, res) =>{    
     const nombre = req.body.nombre;
     const precio = req.body.precio;
     const descripcion = req.body.descripcion;
 
-    let datos ={ 
+    let datos = {
         nombre: nombre,
         precio: precio,
         descripcion: descripcion
-    };
+    }
 
-    let SQL = "insert into productos set ?";
+    let sql = "INSERT INTO productos set ?";
 
-    conexion.query(SQL, datos, function(err){
+    conexion.query(sql, datos, function(err){
         if (err) throw err;
-            res.render('formulario') 
-    })
-}) ;
+            console.log(`1 Registro insertado`);
+            res.render('enviado')
+        })
+})
 
 
 app.post('/contacto', (req, res) =>{
     const nombre = req.body.nombre;
     const email = req.body.email;
-    console.log("los datos son: " + nombre + ", " + email);
 
-    //creamos una funcion para enviar el mail
+    //Creamos una función para enviar Email al cliente
     async function envioMail(){
-        //configuramos la cuenta de envío
+        //Configuramos la cuenta del envío
         let transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
@@ -124,16 +123,16 @@ app.post('/contacto', (req, res) =>{
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASSWORD
             }
-                
         });
-        //envío del mail
+
+        //Envío del mail
         let info = await transporter.sendMail({
-            from: process.env.email,
+            from: process.env.EMAIL,
             to: `${email}`,
-            subject: "gracias por suscribirte a nuestra app",
-            html: `Muchas gracias por visitar nuestra página <br>
-                Recibirás nuestras promociones a esta dirección de correo.<br>
-                Buen fin de semana`
+            subject: "Gracias por suscribirte a nuestra App",
+            html:`Muchas gracias por visitar nuestra página <br>
+            Recibirás nuestras promociones a esta dirrección de correo. <br>
+            Buen fin de semana!!`
         })
     }
 
@@ -141,23 +140,62 @@ app.post('/contacto', (req, res) =>{
         nombre: nombre,
         email: email
     }
-    
-    let sql = "insert into contacto set ?";
-    
-    conexion.query(sql, datos, function(error){
-        if (error) throw error;
-        console.log('1 registro insertado');
-        envioMail().catch(console.error);
-    })
-    
-    res.render('contacto');
-});
-   
 
+    let sql = "INSERT INTO contacto set ?";
 
-// servidor a la escucha de las peticiones
-app.listen(APP_PORT, ()=>{
-    console.log(`Servidor trabajando en 
-    el puerto: ${APP_PORT}`);
+    conexion.query(sql, datos, function(err){
+        if (err) throw err;
+            console.log(`1 Registro insertado`);
+            //Email
+            envioMail().catch(console.error);
+            res.render('enviado')
+        })
 
 })
+
+app.post('/delete', (req, res) => {
+
+    console.log(req.body.idProducto);
+
+    let sql = "DELETE FROM productos where idProducto = " + req.body.idProducto + "";
+    console.log(sql);
+    conexion.query(sql, function(err, result){
+        if (err) throw err;
+            console.log('Dato eliminado: ' + result.affectedRows);
+            res.render('formulario')
+    })
+})
+
+app.post('/update', (req, res) => {
+
+    const nombre =req.body.nombre;
+    const precio =req.body.precio;
+    const descripcion =req.body.descripcion;
+    const idProducto =req.body.id;
+
+    let sql = "UPDATE productos SET nombre='"+ nombre +"',precio= '"+ precio +", descripcion= '"+descripcion+"'    WHERE idProducto = "+req.body.id+
+    
+    console.log(sql);
+    conexion.query(sql, function(err, result){
+        if (err) throw err;
+            console.log('Dato actualizado ' + result.affectedRows);
+            res.render('formulario')
+    })
+})
+
+//Servidor a la escucha de las peticiones
+app.listen(PORT, ()=>{
+    console.log(`Servidor trabajando en el Puerto: ${PORT}`);
+})
+ 
+
+
+
+
+
+
+
+
+
+
+
